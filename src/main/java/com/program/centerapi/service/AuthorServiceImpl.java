027b3a079aa73a5f2414ef60386fd974f26f85da
admin <admin@LAPTOP-5V8AISB8>
2017-05-17 19:12:40 +0800

    菜单权限查询

--- src/main/java/com/program/centerapi/service/AuthorServiceImpl.java
+++ src/main/java/com/program/centerapi/service/AuthorServiceImpl.java
@@ -27,7 +27,7 @@ public class AuthorServiceImpl extends GenericServiceImpl implements AuthorServi
 	@Override
 	public List<TbSecqurity> findSecqurity() throws DbException {
 		// TODO Auto-generated method stub
-		String sql = "SELECT s.id AS 'id',s.action AS 'action',s.des AS 'des',s.icon AS 'icon',s.ismenu AS 'ismenu',s.menu_name AS 'menu_name',s.pid AS 'pid' FROM tb_admin_user u LEFT JOIN tb_user_role ur ON u.id = ur.uid LEFT JOIN tb_role r ON ur.rid = r.id LEFT JOIN tb_role_secqurity rs ON r.id = rs.rid LEFT JOIN tb_secqurity s ON s.id = rs.sid";
+		String sql = "SELECT * FROM tb_secqurity";
 		return queryDao.findBySql(sql, TbSecqurity.class, null);
 	}
 
